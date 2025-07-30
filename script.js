e4ad290a8c7b24196c75e97acf009a426a38a68e
Martosky <imoniaromartins@yahoo.com>
2025-07-30 23:26:04 +0100

    created the random choice

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const resultMsg = document.getElementById("result-msg");
 const winnerMsg = document.getElementById("winner-msg");
 const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorBtn = document.getElementById("scissor-btn");
+const options = ["rock", "paper", "scissor"];
+
+const rendomCOmputerChoice = () => {
+    const ramdomNum = Math.floor(Math.random()* options.length)
+    return options[ramdomNum];
+ }
