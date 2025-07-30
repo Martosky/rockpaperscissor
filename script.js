1baafb57448c76b6a602446787f74556e7698d80
Martosky <imoniaromartins@yahoo.com>
2025-07-30 22:03:48 +0100

    got the domNods

---
+++ script.js
@@ -0,0 +1,5 @@
+const resetBtn = document.getElementById("reset-btn");
+const resultMsg = document.getElementById("result-msg");
+const winnerMsg = document.getElementById("winner-msg");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
